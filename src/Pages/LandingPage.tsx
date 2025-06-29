224cac6d5c300b0800c14d5d3eb6dc85f36044e7
pheebee <adejumohtoheebpheebee@gmail.com>
2025-06-29 18:12:31 +0100

    abdroid build _fix

---
+++ src/Pages/LandingPage.tsx
@@ -0,0 +1,28 @@
+import Features from "../Components/Features";
+import Hero from "../Components/Hero";
+import HowItWorks from "../Components/HowItWork";
+import Navbar from "../Components/NavBar";
+import OurApp from "../Components/OurApp";
+import OurGoalsSection from "../Components/OurGoals";
+import Action from "../Components/Action";
+import ContactSection from "../Components/ContactUs";
+import Footer from "../Components/Footer";
+
+
+const LandingPage = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Hero/>
+            <Features/>
+            <HowItWorks/>
+            <OurApp/>
+            <OurGoalsSection/>
+            <Action/>
+            <ContactSection/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default LandingPage
